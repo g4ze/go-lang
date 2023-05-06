362679d126ff7dccb15fde8534707bd4229d3a15
g4ze <bhaiyajionline17@gmail.com>
2023-05-06 17:56:33 +0530

    aded memory management boiler plate code

---
+++ memorymanagement05/go.mod
@@ -0,0 +1,3 @@
+module memorymanagement05
+
+go 1.18
---
+++ memorymanagement05/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	linechange := "\n================================\n"
+
+	fmt.Println("Heloooiiii" + linechange + "Welcome to structs in go-lang")
+}
